1c1f73e0c034a3c63d9b451f2cf4161fe6a45e06
User <kjk020913@gmail.com>
2026-03-19 14:46:36 +0900

    feat: friend_bfs.py

---
+++ day01/friend_bfs.py
@@ -0,0 +1,14 @@
+fr_info = {
+    'Summer': ['John', 'Justin', 'Mike'],
+    'John':   ['Summer', 'Justin'],
+    'Justin': ['John', 'Summer', 'Mike', 'May'],
+    'Mike':   ['Summer', 'Justin'],
+    'May':    ['Justin', 'Kim'],
+    'Kim':    ['May'],
+    'Tom':    ['Jerry'],
+    'Jerry':  ['Tom']
+}
+
+print("Summer의 친구:", fr_info['Summer'])
+
+print("May의 친구:", fr_info['May'])
